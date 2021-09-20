a5d99750983477530ae86f87fae6b03d6f9ee42c
helciodev <helcioandre@live.com>
2021-09-20 23:20:12 +0100

    Install all necessary dependencies

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Link, Route } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd'
+function App(){
+  return (
+    <div className="App">
+      <div className="navbar">
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
+    </div>
+  )
+}
+
+export default App;
