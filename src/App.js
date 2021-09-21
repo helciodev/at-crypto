2a07a1add2c5bfb6bbf9c7b99a5544e87ddb8680
helciodev <helcioandre@live.com>
2021-09-21 10:33:58 +0100

    Install react-router-dom dependencie

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Switch, Link, Route } from 'react-router-dom';
-import { Layout, Typography, Space } from 'antd'
+import { Layout, Typography, Space } from 'antd';
+import { Navbar } from './components';
+
 function App(){
   return (
     <div className="App">
       <div className="navbar">
+        <Navbar />
       </div>
       <div className="main"></div>
       <div className="footer"></div>
